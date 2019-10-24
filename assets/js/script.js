f6ab40847392fc9b6079fb4514bd9c6fae49bd3f
Sofiag19 <sofia.giacometti.19@gmail.com>
2019-10-24 14:51:38 +0200

    impostazione ciclo

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,19 @@
 // e per i multipli di 5 stampi Buzz.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
-// stampare numeri da 1 a 100 - devo trovare un metodo per stampare i numeri da 1 a 100
+// stampare numeri da 1 a 100
 var input = 1;
 while (input <= 100) {
  console.log(input);
+ // sovrascrivere numeri multipli di 3 e 5 - uso il modulo var%3==0 && var%5==0
+ if (((input%3)==0)&&((input%5)==0)) {
+   console.log(input=fizz);
+   // sovrascrivere numeri multipli di 5 - uso il modulo var%5==0
+ } else if ((input%5)==0) {
+   console.log();
+   // sovrascrivere numeri multipli di 3 - uso il modulo var%3==0
+ } else if ((input%3)==0){
+   console.log();
+ }
  input++;
 }
-// sovrascrivere numeri multipli di 3 - uso il modulo var%3==0
-
-// sovrascrivere numeri multipli di 5 - uso il modulo var%5==0
-
-// sovrascrivere numeri multipli di 3 e 5 - uso il modulo var%3==0 && var%5==0
